6ec74d598f545d187c289135fece9b1aa66ec4fd
Alejandro Acebes <aacebes@autentia.com>
2017-11-06 21:53:44 +0100

    Created builder of enviromental factor thresholds from a formatted string

--- src/main/java/org/opentripplanner/api/common/EnvironmentalFactorsThresholdsBuilder.java
+++ src/main/java/org/opentripplanner/api/common/EnvironmentalFactorsThresholdsBuilder.java
@@ -1,35 +1,92 @@
 package org.opentripplanner.api.common;
 
-import org.apache.commons.math3.util.Pair;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.opentripplanner.routing.constraints.EnvironmentalFactorThreshold;
 import org.opentripplanner.routing.constraints.EnvironmentalFactorType;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
-
+/**
+ * Builder that creates a list of EnvironmentalFactorThreshold from an input String of ampersand
+ * separated elements that represent the threshold of one of the allowed properties (average or
+ * peak) for the specific environmental factor.
+ * 
+ * Each of the elements of the given string should have the following format:
+ * ENVIROMENTAL_{factorType}_{propertyName (MAX_AVERAGE or MAX_PEAK)}:{value}
+ * 
+ * Any element with other format will be ignored.
+ * 
+ * Here is an example of a valid input:
+ * ENVIRONMENTAL_POLLUTION_MAX_AVERAGE=5.0&ENVIRONMENTAL_POLLUTION_MAX_PEAK=15.0&ENVIRONMENTAL_ALLERGIC_MAX_AVERAGE=10.0
+ */
 public class EnvironmentalFactorsThresholdsBuilder {
+   
+    public static List<EnvironmentalFactorThreshold> build(String inputString) {
+        final List<EnvironmentalFactorThreshold> thresholds = new ArrayList<>();
 
-        /*
-         &ENVIRONMENTAL_POLLEN_MAX_AVERAGE=3
-         &ENVIRONMENTAL_POLLEN_MAX_PEAK=4
-         &ENVIRONMENTAL_POLLUTION_MAX_AVERAGE=3
-         &ENVIRONMENTAL_POLLUTION_MAX_PEAK=4
-
-
-         ENVIRONMENTAL -> TYPE: POLLEN
-         MAX_PEAK: 4
-         MAX_AVERAGE: 3
-         TYPE: POLLUTION
-         MAX_PEAK: 4
-         MAX_AVERAGE: 3
-         */
-
-        Map<EnvironmentalFactorType, Pair<Double, Double>> weightsPerFactor = Collections
-                .singletonMap()
-
-        public List<EnvironmentalFactorThreshold> builder(String routingRequest) {
-
+        if (inputString != null) {
+            String[] factorProperties = inputString.split("&");
+            for (String factorProperty : factorProperties) {
+                processNewProperty(thresholds, factorProperty);
+            }
         }
+
+        return thresholds;
+    }
+
+    private static void processNewProperty(final List<EnvironmentalFactorThreshold> thresholds,
+            String factorProperty) {
+        String[] tokens = factorProperty.split("_");
+        if (tokens.length == 4 && "ENVIRONMENTAL".equals(tokens[0])) {
+            try {
+                EnvironmentalFactorType type = EnvironmentalFactorType.valueOf(tokens[1]);
+                int thresholdIndex = getPositionOfThresholdOfType(thresholds, type);
+                String[] fieldValue = tokens[3].split("=");
+
+                if (thresholdIndex == -1) {
+                    addNewFactorThreshold(thresholds, type, fieldValue);
+                } else {
+                    replaceWithCopyWithNewProperty(thresholds, type, thresholdIndex, fieldValue);
+                }
+            } catch (IllegalArgumentException e) {
+                // Do nothing for we want to continue.
+            }
+        }
+    }
+
+    private static int getPositionOfThresholdOfType(List<EnvironmentalFactorThreshold> thresholds,
+            EnvironmentalFactorType type) {
+        int i = 0;
+        for (EnvironmentalFactorThreshold threshold : thresholds) {
+            if (type.equals(threshold.getType())) {
+                return i;
+            }
+            i++;
+        }
+        return -1;
+    }
+
+    private static void addNewFactorThreshold(final List<EnvironmentalFactorThreshold> thresholds,
+            EnvironmentalFactorType type, String[] fieldValue) {
+        if ("AVERAGE".equals(fieldValue[0])) {
+            thresholds.add(new EnvironmentalFactorThreshold(type, new Double(fieldValue[1]), null));
+        } else if ("PEAK".equals(fieldValue[0])) {
+            thresholds.add(new EnvironmentalFactorThreshold(type, null, new Double(fieldValue[1])));
+        }
+    }
+
+    private static void replaceWithCopyWithNewProperty(final List<EnvironmentalFactorThreshold> thresholds,
+            EnvironmentalFactorType type, int thresholdIndex, String[] fieldValue) {
+        EnvironmentalFactorThreshold existingThreshold = thresholds.get(thresholdIndex);
+        thresholds.remove(thresholdIndex);
+
+        if ("AVERAGE".equals(fieldValue[0])) {
+            thresholds.add(new EnvironmentalFactorThreshold(type, new Double(fieldValue[1]),
+                    existingThreshold.getMaxPeak()));
+        } else if ("PEAK".equals(fieldValue[0])) {
+            thresholds.add(new EnvironmentalFactorThreshold(type, existingThreshold.getMaxAverage(),
+                    new Double(fieldValue[1])));
+        }
+    }
 
 }
--- src/main/java/org/opentripplanner/routing/constraints/EnvironmentalFactorType.java
+++ src/main/java/org/opentripplanner/routing/constraints/EnvironmentalFactorType.java
@@ -1,7 +1,5 @@
 package org.opentripplanner.routing.constraints;
 
 public enum EnvironmentalFactorType {
-
-        POLLUTION, ALLERGIC, NOISE
-
+    POLLUTION, ALLERGIC, NOISE
 }
